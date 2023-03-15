54a4373b1f85f4f4dbb4b8a2bdbf79e71096905e
mspolermo <mspolermo@gmail.com>
2023-03-15 14:30:47 +0500

    style-code was corrected

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ const todoForm = document.querySelector('#todo-form');
 const addButton = document.querySelector('#add');
 const todoInput = document.querySelector('#todo-input');
 
-
-//Реализация placeholder
 const placeholder = todoInput.getAttribute('data-placeholder');
 
 todoInput.innerHTML === '' && (todoInput.innerHTML = placeholder);
+
 todoInput.addEventListener('focus', function (e) {
     const value = e.target.innerHTML;
     value === placeholder && (e.target.innerHTML = '');
@@ -17,49 +16,8 @@ todoInput.addEventListener('blur', function (e) {
     value === '' && (e.target.innerHTML = placeholder);
 });
 
-
 addButton.addEventListener('click', formHandler);
 
-function formHandler(event) {
-    const taskText = todoInput.innerText; // Получаем значение введенное в Input поле 
-    if (todoInput.innerText=='Введите название задачи') {
-    }else{
-     
-    const newTask = document.createElement('li'); // Создаем тег li с помощью создания элемента
-    //     //Создаем кнопку удалить
-    // const deleteBtn = document.createElement('button');
-    // deleteBtn.setAttribute('role', 'button');
-    // deleteBtn.classList.add('btnDel');
-    // deleteBtn.innerText = 'o';
-
-        //Создаем кнопку удалить
-    const deleteBtn = document.createElement('div');
-    deleteBtn.setAttribute('role', 'button');
-    deleteBtn.classList.add('btnDel');
-
-
-
-    newTask.append(deleteBtn);
-
-    newTask.append(taskText); //Вставляем значение полученные из инпут поля
-    deleteBtn.addEventListener('click', deleteTask) //вешаем функцию удаления по клику на кнопку Х
-    
-    if (taskText !== "") {
-    todoList.insertAdjacentElement('afterbegin', newTask); //Добавляем элемент на страницу
-    todoInput.innerText=''; // очищаем поле ввода
-    todoInput.focus();} //оставляем поле ввода активным
-
-  }
-};
-
-
-function deleteTask() {
-  this.closest('li').remove();/* через this обращаемся к объекту, на котором висит функция (кнопка x).
-  через closets ищим ближайший родительский элемент у этого объекта (для кнопки x - тег li с текстом таски),
-  удаляем этот элемент */
-}
-
-//Реализация кнопки "Центр"
 const btnCener = document.querySelector('#btnCenter')
 const todo = document.querySelector('#simple-todo')
 let counter = 0;
@@ -72,10 +30,37 @@ btnCener.addEventListener('click', function(){
   }
 });
 
-//создание задачи по клавише Enter
 todoInput.addEventListener('keydown', (event) => {
     if (event.keyCode == 13) {
       event.preventDefault();
       formHandler();
-    }
-  })
+    };
+  });
+
+function formHandler(event) {
+    const taskText = todoInput.innerText; 
+    if (todoInput.innerText=='Введите название задачи') {
+    }else{
+     
+    const newTask = document.createElement('li');
+
+    const deleteBtn = document.createElement('div');
+    deleteBtn.setAttribute('role', 'button');
+    deleteBtn.classList.add('btnDel');
+
+    newTask.append(deleteBtn);
+    newTask.append(taskText); 
+
+    deleteBtn.addEventListener('click', deleteTask)
+    
+    if (taskText !== "") {
+      todoList.insertAdjacentElement('afterbegin', newTask); 
+      todoInput.innerText=''; 
+      todoInput.focus();
+    };
+  };
+};
+
+function deleteTask() {
+  this.closest('li').remove();
+};  
